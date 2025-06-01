7bb0b70b4afeaf8e635a3e2cb400d4c31aacb298
Leon2213 <48647601+Leon2213@users.noreply.github.com>
2025-06-01 14:36:24 +0200

    first

---
+++ receiver-backend/receiver-backend/src/main/java/com/example/receiver_backend/ReceiverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.receiver_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiverBackendApplication.class, args);
+	}
+
+}
